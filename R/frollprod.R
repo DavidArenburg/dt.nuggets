10eba67f36fc484b1ebf268945fa78a28c74dfbd
David Arenburg <david.arenburg@gmail.com>
2018-07-19 16:27:44 +0300

    Update frollprod.R

--- R/frollprod.R
+++ R/frollprod.R
@@ -13,18 +13,37 @@
 #' DT <- data.table(x = sample(10), y = sample(1:2, 10, replace = TRUE), key = "y")
 #' frollprod(DT, "x", 3, by = "y", type = "lead")
 
-frollprod <- function(DT, col, N, Name , by, partial = FALSE, ...){
+frollprod <- function(DT, col, N, Name , by, partial = FALSE, na.rm = FALSE, ...){
   
   if(missing(Name)) new_col <- paste0(col, "_prod_", N)  else new_col <- Name
-    
+  
   if(partial) fill. <- 1L else fill. <- NA
   
   if(missing(by)) {
     
-    return(DT[, (new_col) := Reduce(`*`, shift(eval(as.name(col)), 0L:(N - 1L), fill = fill., ...))])
+    if(na.rm) {
+      
+      DT[, (new_col) := matrixStats::rowProds(as.matrix(setDT(shift(eval(as.name(col)), 0L : (N - 1L), ...))), na.rm = TRUE)]
+      
+      if(!partial) DT[1L:(N - 1L), (new_col) := NA_real_]
+      
+    } else DT[, (new_col) := Reduce(`*`, shift(eval(as.name(col)), 0L:(N - 1L), fill = fill., ...))]
+    
+  } else {
+    
+    if(na.rm) {
+      
+      DT[, (new_col) := matrixStats::rowProds(as.matrix(setDT(shift(eval(as.name(col)), 0L : (N - 1L), ...))), na.rm = TRUE), by = by]
+      
+      if(!partial) {
+        
+        indx <- seq_len(N - 1L)
+        DT[, (new_col) := replace(eval(as.name(new_col)), indx, NA_real_), by = by]
+        
+      }
+      
+    } else DT[, (new_col) := Reduce(`*`, shift(eval(as.name(col)), 0L:(N - 1L), fill = fill., ...)), by = by]
     
   }
-  
-  DT[, (new_col) := Reduce(`*`, shift(eval(as.name(col)), 0L:(N - 1L), fill = fill., ...)), by = by]
   
 }
