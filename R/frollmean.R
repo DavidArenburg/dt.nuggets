b4d6ad9ca9d700fef66f1d5639b0b922b48adc09
David Arenburg <david.arenburg@gmail.com>
2018-07-19 15:54:38 +0300

    Update frollmean.R

--- R/frollmean.R
+++ R/frollmean.R
@@ -13,13 +13,19 @@
 #' DT <- data.table(x = sample(10), y = sample(1:2, 10, replace = TRUE), key = "y")
 #' frollmean(DT, "x", 3, by = "y", type = "lead")
 
-frollmean <- function(DT, col, N, Name, by, partial = FALSE, ...){
+frollmean <- function(DT, col, N, Name, by, partial = FALSE, na.rm = FALSE, ...){
   
   if(missing(Name)) new_col <- paste0(col, "_mean_", N)  else new_col <- Name
   
   if(missing(by)) {
     
-    if(partial) {
+    if(na.rm) {
+      
+      DT[, (new_col) := rowMeans(setDT(shift(eval(as.name(col)), 0L : (N - 1L), ...)), na.rm = TRUE)]
+      
+      if(!partial) DT[1L:(N - 1L), (new_col) := NA_real_]
+      
+    } else if(partial) {
       
       DT[, (new_col) := as.numeric(Reduce(`+`, shift(eval(as.name(col)), 0L : (N - 1L), fill = 0, ...)))]
       indx <- seq_len(N)
@@ -29,13 +35,24 @@ frollmean <- function(DT, col, N, Name, by, partial = FALSE, ...){
     } else DT[, (new_col) := Reduce(`+`, shift(eval(as.name(col)), 0L:(N - 1L), ...)) / N]
     
   } else {
+    
+    if(na.rm){
       
-    if(partial) {
+      DT[, (new_col) := rowMeans(setDT(shift(eval(as.name(col)), 0L : (N - 1L), ...)), na.rm = TRUE), by = by]
+      
+      if(!partial) {
+        
+        indx <- seq_len(N - 1L)
+        DT[, (new_col) := replace(eval(as.name(new_col)), indx, NA_real_), by = by]
+        
+      }
+        
+    } else if(partial) {
       
       DT[, (new_col) := as.numeric(Reduce(`+`, shift(eval(as.name(col)), 0L : (N - 1L), fill = 0, ...))), by = by]
       indx <- seq_len(N)
       DT[, (new_col) := c(eval(as.name(new_col))[indx] / indx, eval(as.name(new_col))[-indx] / N), by = by]
-        
+      
     } else DT[, (new_col) := Reduce(`+`, shift(eval(as.name(col)), 0L : (N - 1L), ...)) / N, by = by]
     
   }
